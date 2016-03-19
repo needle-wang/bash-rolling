4693c4acb846dde5d73fb581f38a715b31635b61
needle <needlewang2011@gmail.com>
2016-03-19 17:42:24 +0800

    add check git status

---
+++ check_gi_st.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -
+#查看vim的vundle管理的插件的git status
+#2016年 03月 18日 星期五 15:05:52 CST
+
+cur="$HOME/.vim/bundle"
+
+if [[ "$1" ]]; then
+    cur=$1
+fi
+
+cd $cur || exit 1
+
+cur=$(pwd)
+for i in *
+do
+    gitname=${cur}/${i}
+    cd ${gitname} || continue
+
+    result=$(git status)
+
+    if ! echo "${result}" | grep -q "干净的工作区"; then
+        echo "----- ${gitname} -----"
+        echo "${result}"
+        echo
+    fi
+done
+
