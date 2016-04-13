1a8b6dff6e2fd689a10caa6500f8e263bc0768ef
needle <needlewang2011@gmail.com>
2016-04-13 16:06:50 +0800

    add autojump

--- soft_for_new_OS.sh
+++ soft_for_new_OS.sh
@@ -2,7 +2,7 @@
 #新装ubuntu时要装的软件
 #2014年 07月 26日 星期六 18:04:42 CST
 
-sudo apt-get install openssh-server openssh-client iotop atop iftop aria2 curl lftp cmake ipython python-dev python-pip zhcon fcitx fcitx-table-wubi git gitk git-cola manpages-zh tree ascii libncurses5-dev unrar aptitude goldendict mysql-server mysql-workbench mailutils fping exuberant-ctags smplayer vlc dos2unix nodejs silversearcher-ag lrzsz libjpeg-dev traceroute
+sudo apt-get install openssh-server openssh-client iotop atop iftop aria2 curl lftp cmake ipython python-dev python-pip zhcon fcitx fcitx-table-wubi git gitk git-cola manpages-zh tree ascii libncurses5-dev unrar aptitude goldendict mysql-server mysql-workbench mailutils fping exuberant-ctags smplayer vlc dos2unix nodejs silversearcher-ag lrzsz libjpeg-dev traceroute autojump
 
 #gimp
 
