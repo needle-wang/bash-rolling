20fbe9c59fbaf3f2b85daa887c457e512d43b854
needle <needlewang2011@gmail.com>
2020-11-25 20:22:28 +0800

    update gengxin.sh

--- gengxin.sh
+++ gengxin.sh
@@ -1,6 +1,30 @@
 #!/bin/bash -
+# 2020年 05月 16日 星期六 01:16:54 CST
 
-trap "echo '中断';exit 1" INT
+trap "echo -e '\n手动中断...';exit 1" INT
 #LANG=C 
-sudo apt-get update -y
-sudo apt-get dist-upgrade -y
+
+if which apt-fast > /dev/null; then
+  APT='sudo apt-fast'
+else
+  APT='sudo apt-get'
+fi
+
+if [[ "$1" ]]; then
+  PROXY="-c \"$HOME/bin/apt-proxy.conf\""
+else
+  PROXY=""
+fi
+
+$APT update -y
+$APT dist-upgrade -y "$PROXY"
+
+echo -e '\n2秒后 进行apt autoclean, 按任意键取消...\n'
+
+if ! read -rt 2; then
+  $APT autoremove
+  $APT autoclean
+  $APT clean
+  sudo updatedb
+fi
+
